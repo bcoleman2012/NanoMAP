2d43a1b4a3985245db08efbda7ffcb5df06a3b10
Ben Coleman <bcoleman2012@fau.edu>
2017-06-10 16:43:05 -0400

    Initial commit

---
+++ NanoFirmware.ino
@@ -0,0 +1,178 @@
+// TO DO: make forward() backward() functions 
+/* Nano MAP firmware. 
+ *  Manual control buttons on pins 4 and 5
+ *  Stepper motor pulse pin on 2
+ *  Stepper motor direction pin on 3
+ * 
+ * TO DO: 
+ * Implement forward() and backward() functions (nice abstraction)
+ * Create automation framework. Firmware state machine needs done. 
+ * 
+ * 
+ * 
+ * 
+ * USAGE: 
+ * 
+ * If USB is connected and a serial connection is available, the program
+ * will listen at 9600 baud for commands. The commands are: 
+ * b - backward N steps
+ * f - forward N steps
+ * o - oscillate forward N steps, and backward N steps
+ * 
+ * The commands require a flag afterward, in the form of an integer. 
+ * A valid command is
+ * b100\n
+ * (i.e. type 'b' '1' '0' '0', then press enter
+ * This will take it backward 100 steps. 
+ * 
+ * If USB is not connected, then the pushbuttons on pin 4 and 5 control 
+ * the stepper motor slide. 
+ * 
+ * Note that limit switches are not yet implemented. Therefore, be careful with
+ * what you ask the stepper motor to do!
+ */
+
+
+const int stepPin = 2;
+const int directionPin = 3;
+const int manualForward = 4; 
+const int manualBackward = 5; 
+
+
+
+void pulse(int pin, int len){
+  // PIN is a digital output pin. Len is the pulse width in ms
+  digitalWrite(pin,HIGH);
+  delay(len);
+  digitalWrite(pin,LOW);
+  return;
+}
+
+
+
+
+
+void setup() {
+ 
+  pinMode(stepPin,OUTPUT);
+  pinMode(directionPin,OUTPUT);
+
+  pinMode(manualForward,INPUT);
+  pinMode(manualBackward,INPUT);
+
+  analogReference(EXTERNAL);
+  // analogReference is necessary so that the system can run off of 
+  // the attached power supply / regulator pair, even when USB 5.0V
+  // is attached
+  
+  digitalWrite(directionPin,HIGH);
+  digitalWrite(stepPin,LOW);
+
+  Serial.begin(9600);
+}
+
+
+bool homeAxis(){
+  }; // limit switches not implemented in hardware yet
+
+
+ // static (global) ints for hacky command line interface
+ int command = 0;
+ int serialValue = 0;
+ 
+void loop() {
+  int direction_status = 0; // 1 corresponds to high directino pin
+ if( Serial.available())
+ {
+   Serial.print("READY\n"); 
+   char ch = Serial.read();
+   if (ch == 'b'){
+    Serial.print("B \n");
+    command = 1; // backward n steps
+   }
+   else if (ch == 'f'){
+    Serial.print("F \n");
+    command = 2; // forward n steps
+   }
+   else if (ch == 'o'){
+    Serial.print("O \n");
+    command = 3; // oscillate forward then back
+   }
+   else if(ch >= '0' && ch <= '9')   {           // is ch a number?  
+     serialValue = serialValue * 10 + ch - '0';           // yes, accumulate the integer value in serialValue
+
+     Serial.print("\n");
+   }
+   else  // its not a digit so terminate the input    
+   {
+       if (command == 1){ // backward
+        Serial.print("B ");
+        Serial.print(serialValue);
+        Serial.print(" steps\n");
+        digitalWrite(directionPin,HIGH);
+        for (int i = 0; i<serialValue; i++){
+        // Serial.print("step\n");
+        pulse(stepPin,1);
+        delay(10);
+      }
+       }
+       else if (command == 2){
+         Serial.print("F ");
+        Serial.print(serialValue);
+        Serial.print(" steps\n");
+      digitalWrite(directionPin,LOW);
+        for (int i = 0; i<serialValue; i++){
+      //Serial.print("step\n");
+        pulse(stepPin,1);
+        delay(10);
+      }
+       }
+       else if (command == 3){
+        Serial.print("Osc ");
+        Serial.print(serialValue);
+        Serial.print(" steps\n");
+      digitalWrite(directionPin,LOW);
+        for (int i = 0; i<serialValue; i++){
+        // Serial.print("step\n");
+        pulse(stepPin,1);
+        delay(10);
+      }
+      digitalWrite(directionPin,HIGH);
+        for (int i = 0; i<serialValue; i++){
+        // Serial.print("step\n");
+        pulse(stepPin,1);
+        delay(10);
+      }
+       }
+       Serial.print("OP: ");
+       Serial.print(serialValue);
+       Serial.print("\t");
+       Serial.print(command);
+       Serial.print("\n");
+       serialValue = 0;
+       command = 0;
+       Serial.print("EXIT\n"); 
+   }
+ }
+ else 
+ {
+  // if serial is not available, let's go for hardware actuation
+
+  if (digitalRead(manualForward) == LOW)
+  {
+    // lets go forward
+    digitalWrite(directionPin,LOW);
+    pulse(stepPin,2);
+    delay(8);
+  }
+  if (digitalRead(manualBackward) == LOW)
+  {
+    digitalWrite(directionPin,HIGH);
+    pulse(stepPin,2);
+    delay(8);
+  }
+ }
+}
+
+ 
+
---
+++ NanoMAP.py
@@ -0,0 +1,117 @@
+import time
+import sys
+import serial
+import serial.tools.list_ports
+
+# SessionManager provides an interface for string-based messages and the arduino device
+# NanoMAPController provides interface for scripting support
+# gcode-style input files will be supported by NanoMAPController
+
+class SessionManager: 
+	""" Simple wrapper class for an Arduino serial connection session """
+	def __init__(self, verbose = False): 
+		self._verbose = verbose
+		self._arduino_ports = []
+		self._arduino_serial_session = None
+		self._selected_port = None
+		self._serial_session = None
+		if self._verbose: 
+			print "SessionManager verbose mode activated"
+
+	def scanPorts(self): 
+		if self._verbose: 
+			for p in serial.tools.list_ports.comports():
+				print p
+
+		self._arduino_ports = [p.device for p in serial.tools.list_ports.comports() if 'Arduino' in p.description]
+		if ((len(self._arduino_ports) == 0) and self._verbose): 
+			print "Error: Arduino device not found"
+
+	def initSession(self, baudrate = 9600): 
+		if (len(self._arduino_ports) != 0): 
+			self._selected_port = self._arduino_ports[0]
+
+			start_indicator = "READY\n" # needs replaced with better code in next firmware iteration
+			
+			if self._verbose: 
+				print "Attempting serial connection with Arduino device on",self._selected_port
+			
+			self._serial_session = serial.Serial(self._selected_port, baudrate, timeout = 0.1)
+			timeout = 10
+			start_time = time.time()
+
+			if self._verbose: 
+				print "Serial connection established, attempting firmware handshake"
+
+			while (time.time() < start_time + timeout) and (self._serial_session.readline() != start_indicator):
+				pass
+
+			# next time I have access, implement useful verbose logs for timeout reached and success condition
+			# for some reason the following hangs: 
+			# 	if (self._serial_session.readline() == start_indicator): 
+			# 		if self._verbose: 
+			# 			print "Successful connection established with device at " + self._selected_port
+			# 		return
+			# if self._verbose: 
+			# 	print "Unable to make a connection. Timeout exceeded or device not found."
+
+	def write(self, message): 
+		if self._serial_session is None: 
+			return
+		if self._verbose: 
+			print "Sending message: ", message
+		self._serial_session.write(message)
+
+	def readline(self): 
+		data = self._serial_session.readline()
+		if self._verbose: 
+			print "Received: ", data
+		return data
+
+	def sendCommand(self, command): 
+		""" This method implements the wait function for the script. Expects newline at end of command"""
+		endStepIndicator = "EXIT\n"
+		self.write(command)
+		while (self.readline() != endStepIndicator): 
+			if self._verbose: 
+				print '*', 
+		if self._verbose: 
+			print "\nExecuted cmd: ",command 
+
+
+class NanoMAPController: 
+	""" Interface class between the SessionManager and the script input. Construct with a valid, connected SessionManager"""
+	def __init__(self, session_manager): 
+		self._session_manager = session_manager
+		self._script = []
+
+	def parseScriptFromFile(self, filename): 
+		infile = open(filename, 'r')
+		for cmd in infile:
+			if ('#' not in cmd) and (cmd != "\n"): 
+				if cmd[-1] != '\n': 
+					cmd = cmd + '\n'
+				self._script.append(cmd)
+
+	def checkCommand(self,cmd): 
+		""" checks if command is valid """
+		return True
+
+	def script(self): 
+		for cmd in self._script: 
+			self._session_manager.sendCommand(cmd)
+
+	def parseScriptFromList(self, cmd_list): 
+		for cmd in script_list: 
+			self._session_manager.sendCommand(cmd)
+
+
+
+if __name__ == "__main__":
+	sessionManager = SessionManager(verbose = True)
+	sessionManager.scanPorts()
+	sessionManager.initSession()
+
+	interfaceController = NanoMAPController(sessionManager)
+	interfaceController.parseScriptFromFile("cmd.txt")
+	interfaceController.script()
