b23a5881bb6ef53df9398d0df022c36c008eb999
unknown <bcoleman2012@fau.edu>
2017-08-04 18:18:52 -0400

    Added multithreading for MUX actuation. Fixed client always choosing lowest number com device issue.

--- NanoMAP.py
+++ NanoMAP.py
@@ -2,6 +2,7 @@ import time
 import sys
 import serial
 import serial.tools.list_ports
+from threading import Thread
 
 # SessionManager provides an interface for string-based messages and the arduino device
 # NanoMAPController provides interface for scripting support
@@ -9,14 +10,14 @@ import serial.tools.list_ports
 
 class SessionManager: 
 	""" Simple wrapper class for an Arduino serial connection session """
-	def __init__(self, verbose = False): 
+	def __init__(self, verbose = False, selected_port = None): 
 		self._verbose = verbose
 		self._arduino_ports = []
 		self._arduino_serial_session = None
-		self._selected_port = None
+		self._selected_port = selected_port
 		self._serial_session = None
 		if self._verbose: 
-			print "SessionManager verbose mode activated"
+			print "SessionManager verbose mode activated" 
 
 	def scanPorts(self): 
 		if self._verbose: 
@@ -24,12 +25,38 @@ class SessionManager:
 				print p
 
 		self._arduino_ports = [p.device for p in serial.tools.list_ports.comports() if 'Arduino' in p.description]
-		if ((len(self._arduino_ports) == 0) and self._verbose): 
-			print "Error: Arduino device not found"
+		
+		if len(self._arduino_ports) == 1:
+			self._selected_port = self._arduino_ports[0]
+
+		elif (len(self._arduino_ports) <= 0): 
+			print "Error: Arduino device not found. Are you using a knockoff?"
+			knockoff = raw_input("(Y/N):")
+			if knockoff.strip().lower() == "y":
+				print "Select a device from the following list:"
+				for p in serial.tools.list_ports.comports():
+					print "Device:",p.device,"\tDescription:",p.description
+				comport = raw_input("Enter the selected device (as shown above): ")
+				self._arduino_ports = [p.device for p in serial.tools.list_ports.comports() if comport.lower() in p.description.lower()]
+				self._selected_port = self._arduino_ports[0]
+
+			elif knockoff.strip().lower() == "n":
+				print "Please check to ensure that the Arduino device is attached."
+		else: 
+			print "Multiple Arduino devices found. Choose a device from the list: "
+			for p in serial.tools.list_ports.comports():
+				if 'Arduino' in p.description: 
+					print "Device:",p.device,"\tDescription:",p.description
+			comport = raw_input("Enter the selected device (as shown above): ")
+			for p in self._arduino_ports: 
+				if comport.lower() in p.lower(): 
+					self._selected_port = p
+					if self._verbose: 
+						print "Connected with selected port:",self._selected_port
+
 
 	def initSession(self, baudrate = 9600): 
-		if (len(self._arduino_ports) != 0): 
-			self._selected_port = self._arduino_ports[0]
+		if (self._selected_port is not None): 
 
 			start_indicator = "r" # needs replaced with better code in next firmware iteration
 			
@@ -43,8 +70,16 @@ class SessionManager:
 			if self._verbose: 
 				print "Serial connection established, attempting firmware handshake"
 
-			while (time.time() < start_time + timeout) and (self._serial_session.readline() != start_indicator):
-				pass
+			curr_time = time.time()
+			while (curr_time < start_time + timeout) and (self._serial_session.readline() != start_indicator):
+				curr_time = time.time()
+
+			if curr_time > start_time + timeout: 
+				if self._verbose: 
+					print "Unable to communicate with device. Is the firmware up to date?" 
+
+		else: 
+			self.scanPorts()
 
 			# next time I have access, implement useful verbose logs for timeout reached and success condition
 			# for some reason the following hangs: 
@@ -106,12 +141,56 @@ class NanoMAPController:
 			self._session_manager.sendCommand(cmd)
 
 
-
-if __name__ == "__main__":
-	sessionManager = SessionManager(verbose = True)
-	sessionManager.scanPorts()
+def runOnPort(port, portList):
+	sessionManager = SessionManager(verbose = False, selected_port = port)
 	sessionManager.initSession()
 
 	interfaceController = NanoMAPController(sessionManager)
 	interfaceController.parseScriptFromFile("cmd.txt")
 	interfaceController.script()
+	try:
+		portList.remove(port)
+	except ValueError: 
+		pass
+
+if __name__ == "__main__":
+
+	ports = [p.device for p in serial.tools.list_ports.comports()]
+	if len(ports) > 1: 
+		# if we have multiple devices attached, break out a separate thread for each one
+		threads = []
+		used_ports = []
+		all_ports = serial.tools.list_ports.comports()
+		while(True):
+
+			if len(used_ports)>=len(all_ports): 
+				continue
+
+			print "Unused devices detected. Would you like to start a new actuation? (CTRL+C to exit)"
+			user_response = raw_input("(Y/N): ")
+
+			if user_response.strip().lower() == "y":
+				all_ports = serial.tools.list_ports.comports()
+				for p in all_ports: 
+					if p.device not in used_ports: 
+						print "Device:",p.device,"\tDescription:",p.description
+				comport = raw_input("Enter the selected device (as shown above): ")
+				for p in all_ports: 
+					if comport.lower() in p.device.lower(): 
+						print "Starting actuation on port",p
+						used_ports.append(p.device)
+						threads.append(Thread(target=runOnPort, args=(p.device, used_ports)))
+						threads[-1].start()
+
+
+
+
+	elif len(ports) == 1:
+		# if we have only one devie attached, then don't worry about it
+		sessionManager = SessionManager(verbose = False)
+		sessionManager.scanPorts()
+		sessionManager.initSession()
+
+		interfaceController = NanoMAPController(sessionManager)
+		interfaceController.parseScriptFromFile("cmd.txt")
+		interfaceController.script()
